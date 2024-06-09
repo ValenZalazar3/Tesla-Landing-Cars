ae4657423a7d34e9e4199f5083c8cc2863afe58a
Valen <valenzalazar3@gmail.com>
2024-06-09 15:20:07 -0300

    fix(root) changes root and ghpages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  base:"http://ValenZalazar3.github.io/Tesla-Landing"
+  // base:""
 });
